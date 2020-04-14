ff55ff6b3543850c70cea12966295720f39e775f
dryan <dryan@users.noreply.github.com>
2020-04-14 16:57:46 -0400

    chainable test for addslashes

--- tests/DjangoFilterString.test.js
+++ tests/DjangoFilterString.test.js
@@ -1,46 +1,66 @@
 const { djangoFilter } = require("../django-filters");
 
-describe("chainable filters", () => {
-  test("apnumber unchained", () => {
+describe("chainable filters: built-in JS methods", () => {
+  test("valueOf", () => {
+    expect(djangoFilter("monkeybat").valueOf()).toBe("monkeybat");
+  });
+
+  test("toString", () => {
+    expect(djangoFilter("monkeybat").toString()).toBe("monkeybat");
+  });
+});
+
+describe("chainable filters: individual methods", () => {
+  test("addslashes", () => {
+    expect(
+      djangoFilter(`"double quotes" and 'single quotes'`)
+        .addslashes()
+        .toString()
+    ).toBe("\\\"double quotes\\\" and \\'single quotes\\'");
+  });
+
+  test("apnumber", () => {
     expect(djangoFilter("1").apnumber().toString()).toBe("one");
   });
 
-  test("intcomma unchained", () => {
-    expect(djangoFilter("1000000").intcomma().toString()).toBe("1,000,000");
-  });
-
-  test("cut unchained", () => {
+  test("cut", () => {
     expect(djangoFilter("this is a string").cut("is").toString()).toBe(
       "th  a string"
     );
   });
 
-  test("trim->cut->slugify", () => {
-    expect(
-      djangoFilter("    this is a string     ").cut("is").slugify().toString()
-    ).toBe("th-a-string");
-  });
-
-  test("date unchained: date only", () => {
+  test("date: date only", () => {
     expect(djangoFilter("21 December 2000").date("N j, Y").toString()).toBe(
       "Dec. 21, 2000"
     );
   });
 
-  test("date unchained: default format", () => {
+  test("date: default format", () => {
     expect(djangoFilter("21 December 2000").date().toString()).toBe(
       "Dec. 21, 2000"
     );
   });
 
-  test("time unchained: default format", () => {
-    expect(djangoFilter("21 December 2000").time().toString()).toBe("midnight");
-  });
-
-  test("date unchained: date and time", () => {
+  test("date: date and time", () => {
     expect(djangoFilter("2011-06-07 13:48").date("N j, Y, P").toString()).toBe(
       "June 7, 2011, 1:48 p.m."
     );
+  });
+
+  test("intcomma", () => {
+    expect(djangoFilter("1000000").intcomma().toString()).toBe("1,000,000");
+  });
+
+  test("time: default format", () => {
+    expect(djangoFilter("21 December 2000").time().toString()).toBe("midnight");
+  });
+});
+
+describe("chainable filters: sequential methods", () => {
+  test("trim->cut->slugify", () => {
+    expect(
+      djangoFilter("    this is a string     ").cut("is").slugify().toString()
+    ).toBe("th-a-string");
   });
 
   test("intcomma->ordinal", () => {
@@ -53,9 +73,5 @@ describe("chainable filters", () => {
     expect(djangoFilter("1000000").ordinal().intcomma().toString()).toBe(
       "1,000,000th"
     );
-  });
-
-  test("valueOf", () => {
-    expect(djangoFilter("monkeybat").valueOf()).toBe("monkeybat");
   });
 });
